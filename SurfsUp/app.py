e24aa246e76668776ca440ba86e55c365ef2a98a
ViolettaAntonova <123307722+ViolettaAntonova@users.noreply.github.com>
2023-04-02 11:01:07 +0100

    complete app

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -9,11 +9,16 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
 
+#################################################
 # create connection to database
+#################################################
 
 # create engine to hawaii.sqlite
 #using absolute rout, because relative gives me an error
 engine = create_engine("sqlite:////Users/violettajanuskevica/Desktop/sqlalchemy_challenge/Resources/hawaii.sqlite")
+#relative path
+#engine = create_engine("sqlite:///../Resources/hawaii.sqlite")
+
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
@@ -33,6 +38,19 @@ app = Flask(__name__)
 # homepage route
 @app.route("/")
 def welcome():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Find the most recent date in the data set.
+    recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+
+    #find latest date available
+    latest_date = session.query(Measurement.date).order_by(Measurement.date).first()
+
+    # Close session
+    session.close()
+
+    #print to homepage
     return (
         f"Welcome to the Hawaii Climate API!<br/>"
         f"Available Routes:<br/>"
@@ -43,6 +61,7 @@ def welcome():
         f"/api/v1.0/start/end<br/>"
         f"<br/>"
         f"note: for start and end dates use format: yyyy-mm-dd<br/>"
+        f"available date range {latest_date[0]} - {recent_date[0]}"
     )
 
 # new rout defining
@@ -50,11 +69,15 @@ def welcome():
 def precipitation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
+
+    #get query for date prcp, for the last 12 months order by date
     year_data = session.query(Measurement.date, Measurement.prcp).\
         filter(Measurement.date >= '2016-08-23').\
             order_by(Measurement.date).all()
+    
     # Close session
     session.close()
+
     # Convert the query results from your precipitation analysis date as the key and prcp as the value.
     year_data_values = []
     for date, prcp in year_data:
@@ -63,6 +86,7 @@ def precipitation():
         precipitation_dict["date"] = date
         year_data_values.append(precipitation_dict)
 
+    #print out
     return jsonify(year_data_values)
 
 # new rout defining
@@ -70,17 +94,23 @@ def precipitation():
 def stations():
     # Create our session (link) from Python to the DB
     session = Session(engine)
-    station_data = session.query(Station.station, Station.id).all()
+
+    #query to get station list
+    station_data = session.query(Station.station, Station.id, Station.name ).all()
+
     # Close session
     session.close()
+
     # Convert the query results from your station analysis as JSON list.
     station_values = []
-    for station, id in station_data:
+    for station, id, name in station_data:
         station_dict = {}
         station_dict["station"] = station
         station_dict["id"] = id
+        station_dict["name"] = name
         station_values.append(station_dict)
 
+    #print values
     return jsonify(station_values)
 
 # new rout defining
@@ -91,12 +121,12 @@ def tobs():
 
     # Find the most recent date in the data set.
     recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-    print(recent_date)
+    #print(recent_date)
     #returns 2017-08-23
 
     # Calculate the date one year from the last date in data set.
     year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-    print(year_ago)
+    #print(year_ago)
     #returns 2016-08-23
 
     # Design a query to find the most active stations (i.e. what stations have the most rows?)
@@ -104,14 +134,18 @@ def tobs():
     active_stat = session.query(Measurement.station, func.count(Measurement.station)).\
         order_by(func.count(Measurement.station).desc()).\
         group_by(Measurement.station).all()
-    print(active_stat[0])
+    #print(active_stat[0])
     #returns ('USC00519281', 2772)
 
+    #Query the dates and temperature observations of the most-active station for the previous year of data.
     active_station = session.query(Measurement.station, Measurement.date, Measurement.tobs).\
         filter(Measurement.station == active_stat[0][0]).\
             filter((Measurement.date >= year_ago)).all()
+    
     # Close session
     session.close()
+
+    # Convert the query results as JSON list.
     active_station_values = []
     for station, date, tobs in active_station:
         active_station_dict = {}
@@ -120,6 +154,8 @@ def tobs():
         active_station_dict["tobs"] = tobs
         active_station_dict
         active_station_values.append(active_station_dict)
+
+    #print result
     return jsonify(active_station_values)
 
 # new rout defining
@@ -127,15 +163,19 @@ def tobs():
 def start_date(start):
     # Create our session (link) from Python to the DB
     session = Session(engine)
+
+    #query Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start.
     sel = [func.avg(Measurement.tobs),
            func.min(Measurement.tobs),
            func.max(Measurement.tobs)
        ]
     tstart = session.query(*sel).\
         filter(Measurement.date >= start).all()
+    
     # Close session
     session.close()
-    # Convert the query results from your station analysis as JSON list.
+
+    # Convert the query results as JSON list.
     start_values = []
     for tavg, tmin, tmax in tstart:
         tstation_dict = {}
@@ -144,6 +184,7 @@ def start_date(start):
         tstation_dict["maximum"] = tmax
         start_values.append(tstation_dict)
 
+    #print results
     return jsonify(start_values)
 
 # new rout defining
@@ -151,6 +192,8 @@ def start_date(start):
 def start_end_date(start, end):
     # Create our session (link) from Python to the DB
     session = Session(engine)
+
+    #Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start-end range.
     sel = [func.avg(Measurement.tobs),
            func.min(Measurement.tobs),
            func.max(Measurement.tobs)
@@ -158,9 +201,11 @@ def start_end_date(start, end):
     tend = session.query(*sel).\
         filter(Measurement.date >= start).\
         filter(Measurement.date <= end).all()
+    
     # Close session
     session.close()
-    # Convert the query results from your station analysis as JSON list.
+
+    # Convert the query results as JSON list.
     end_values = []
     for tavg, tmin, tmax in tend:
         endtstation_dict = {}
@@ -169,6 +214,7 @@ def start_end_date(start, end):
         endtstation_dict["maximum"] = tmax
         end_values.append(endtstation_dict)
 
+    #print results
     return jsonify(end_values)
 
 if __name__ == "__main__":
